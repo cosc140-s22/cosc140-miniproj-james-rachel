bf45db7415136b6ca10f7dea6eabb6dd77c3dbc4
James Cavallo <jcavallo@colgate.edu>
2022-05-05 22:03:26 +0100

    more starter code

--- miniproj/urls.py
+++ miniproj/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, reverse_lazy
+from castlefinder.views import index
+from django.shortcuts import redirect
+
+def root_redirect(request):
+    return redirect(reverse_lazy('castlefinder'))
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('castlefinder/', index),
+    path('', root_redirect)
 ]
