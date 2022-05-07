c62586cf1a7d3117f85373c1b8e77f0f2dd3e913
James Cavallo <jcavallo@colgate.edu>
2022-05-07 12:54:12 +0100

    Added castle stuff

--- castlefinder/views.py
+++ castlefinder/views.py
@@ -27,4 +27,14 @@ def fetchCastles(location, radius, type, keyword):
     payload={}
     headers = {}
     response = requests.request("GET", url, headers=headers, data=payload)
+    dict = response.json()
+    for castle in dict["results"]:
+        print(castle["name"])
+        castle["rating"]
+
+def fetchPhoto():
+    url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=Aap_uEA7vb0DDYVJWEaX3O-AtYp77AaswQKSGtDaimt3gt7QCNpdjp1BkdM6acJ96xTec3tsV_ZJNL_JP-lqsVxydG3nh739RE_hepOOL05tfJh2_ranjMadb3VoBYFvF0ma6S24qZ6QJUuV6sSRrhCskSBP5C1myCzsebztMfGvm7ij3gZT&key=YOUR_API_KEY"
+    payload={}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
     print(response.text)
